49813bf572a0be6d93ef100289d5eb9bed858d7a
javadbat <javadbat@gmail.com>
2024-01-13 12:06:55 +0330

    add generic to bridge

--- lib/Types.ts
+++ lib/Types.ts
@@ -5,8 +5,8 @@ export type JBImagesImageInputElements = {
     image:HTMLImageElement;
 }
 export type JBImageInputValidationErrorTypes = 'REQUIRED' | '';
-export type JBImageInputBridge = {
-    downloader: (url:string, config:JBImageInputConfig) => Promise<string>;
+export type JBImageInputBridge<T = string> = {
+    downloader: (downloaderInput:T, config:JBImageInputConfig) => Promise<string>;
     uploader: (file:File , config:JBImageInputConfig, onProgressCallback?:(percent:number)=>void) => Promise<any>;
 }
 export type JBImageInputConfig = {
